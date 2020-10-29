ee759f67b48ae24b6f252bfb9846289f9ba7d289
Tzong-Lian-Tsay <tzong-lian-tsay@lambdastudents.com>
2020-10-29 14:20:27 -0400

    end of lecture

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 function App() {
+  const [message, setm] = useState("");
+  useEffect(() => {
+    fetch("/api")
+      .then((res) => res.json())
+      .then((data) => setm(data.message));
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">{message}</header>
     </div>
   );
 }
-
 export default App;
